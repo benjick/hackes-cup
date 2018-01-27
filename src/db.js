9d5e03617a14d6c851b4a075e66aa11ac8785275
Max Malm <benjick@dumfan.net>
2018-01-27 13:09:10 +0100

    Add 2018 tournament

--- src/db.js
+++ src/db.js
@@ -1,5 +1,11 @@
 const tournaments = [
   {
+    year: 2018,
+    promote: true,
+    schedule:
+      'http://www.tourneymachine.com/Public/Results/Tournament.aspx?IDTournament=h201711070952114886d487799b3a647',
+  },
+  {
     year: 2017,
     promote: false,
     schedule:
